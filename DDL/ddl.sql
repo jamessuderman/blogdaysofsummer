bdff0820b394b6382d36994da72736b0dc4fb4ae
James Suderman <jamessuderman@gmail.com>
2019-03-24 12:17:56 -0500

    Week 4 Completed

--- DDL/ddl.sql
+++ DDL/ddl.sql
@@ -14,8 +14,25 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema blogsite
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `blogsite`;
+CREATE SCHEMA IF NOT EXISTS `blogsite';
 USE `blogsite` ;
+
+-- -----------------------------------------------------
+-- Table `blogsite`.`Categories`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blogsite`.`Categories` ;
+
+CREATE TABLE IF NOT EXISTS `blogsite`.`Categories` (
+  `Category_Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `Category_Name` VARCHAR(45) NOT NULL,
+  `Created_Date` DATE NULL DEFAULT NULL,
+  `Active_Flag` VARCHAR(1) NULL DEFAULT 'Y',
+  PRIMARY KEY (`Category_Id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 17;
+
+CREATE UNIQUE INDEX `Category_Name_UNIQUE` ON `blogsite`.`Categories` (`Category_Name` ASC);
+
 
 -- -----------------------------------------------------
 -- Table `blogsite`.`Posts`
@@ -25,18 +42,33 @@ DROP TABLE IF EXISTS `blogsite`.`Posts` ;
 CREATE TABLE IF NOT EXISTS `blogsite`.`Posts` (
   `Post_Id` INT(11) NOT NULL AUTO_INCREMENT,
   `Post_Title` VARCHAR(45) NOT NULL,
-  `Category_Id` VARCHAR(45) NULL DEFAULT NULL,
+  `Category_Id` INT(11) NULL DEFAULT NULL,
   `Post_Body` VARCHAR(250) NOT NULL,
   `Post_Date` DATE NULL DEFAULT NULL,
   `Author` VARCHAR(45) NOT NULL,
   `Updated_Date` DATE NULL DEFAULT NULL,
   `Updated_Author` VARCHAR(45) NULL DEFAULT NULL,
   `Deleted_Flag` VARCHAR(1) NULL DEFAULT 'N',
-  PRIMARY KEY (`Post_Id`))
+  PRIMARY KEY (`Post_Id`),
+  CONSTRAINT `Category_Id`
+    FOREIGN KEY (`Category_Id`)
+    REFERENCES `blogsite`.`categories` (`Category_Id`),
+  CONSTRAINT `fk_author`
+    FOREIGN KEY (`Author`)
+    REFERENCES `blogsite`.`users` (`User_Name`),
+  CONSTRAINT `fk_updated_author`
+    FOREIGN KEY (`Updated_Author`)
+    REFERENCES `blogsite`.`users` (`User_Name`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 22;
+AUTO_INCREMENT = 35;
 
 CREATE UNIQUE INDEX `Post_Title_UNIQUE` ON `blogsite`.`Posts` (`Post_Title` ASC);
+
+CREATE INDEX `Category_Id_idx` ON `blogsite`.`Posts` (`Category_Id` ASC);
+
+CREATE INDEX `Author_idx` ON `blogsite`.`Posts` (`Author` ASC);
+
+CREATE INDEX `Updated_Author_idx` ON `blogsite`.`Posts` (`Updated_Author` ASC);
 
 
 -- -----------------------------------------------------
@@ -62,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `blogsite`.`Users` (
   `User_Deleted` VARCHAR(1) NULL DEFAULT 'N',
   PRIMARY KEY (`User_Id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 13;
+AUTO_INCREMENT = 21;
 
 CREATE UNIQUE INDEX `User_name_UNIQUE` ON `blogsite`.`Users` (`User_Name` ASC);
 
